954d6d3b215d2ea3f11bd6dae24394231457eff2
Caio <caio.e.souza@kroton.com.br>
2021-02-19 20:02:18 +0000

    fix: crossOrigin atribute

--- pages/_document.js
+++ pages/_document.js
@@ -11,17 +11,18 @@ class MyDocument extends Document {
       <Html>
         <Head>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
+                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"></link>
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda&family=Great+Vibes&display=swap" rel="stylesheet" />            
+            <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda&family=Great+Vibes&display=swap" rel="stylesheet" />                          
         </Head>
         <body>
           <Main />
           <NextScript />
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-            crossorigin="anonymous"></script>
+            crossOrigin="anonymous"></script>
         </body>
       </Html>
     )
